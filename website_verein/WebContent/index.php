9e0765538618c30a6df1be1a146f4fd06430da64
drlh <leo.hellwich@yahoo.de>
2014-10-03 17:59:10 +0200

    Initial commit

---
+++ website_verein/WebContent/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+ 
+<html>
+ 
+<head>
+    <title>Beispiel HTML5 Grundgerüst - www.html-seminar.de</title>
+    <meta charset="UTF-8" />
+    <meta name="description" content="Kurzbeschreibung" />
+ 
+    <link href="style.css" type="text/css" rel="stylesheet" />
+ 
+</head>
+ 
+<body>
+ 
+</body>
+</html>
