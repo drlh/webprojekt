5bca7d6d220b1bfdc0b006d53204f1ed96a3639b
drlh <leo.hellwich@yahoo.de>
2014-10-08 22:34:23 +0200

    about us

--- website_verein/WebContent/pages/about.php
+++ website_verein/WebContent/pages/about.php
@@ -1,1 +1,29 @@
-About
+<div class="page-header">
+  <h1>Über Uns  <small> - Wir stellen uns vor</small></h1>
+</div>
+<div>
+    <p>Wir sind Bierliebhaber durch und durch. Der Grund weshalb wir diesen Verein gegründet haben ist einfach. <br>
+    Wir wollen die Freude am Biertrinken mit allen Mitmenschen teilen. </p>
+</div>
+<div class="row">
+  <div class="col-xs-6 col-md-6">
+    <div class="thumbnail">
+      <img src="img/about/yannick.jpg">
+      <div class="caption">
+        <h3>Yannick Sarnoch</h3>
+        <p>Vereinsgründer</p>
+        <p><a href="#" class="btn btn-default" role="button">Mehr Infos..</a></p>
+      </div>
+    </div>
+  </div>
+  <div class="col-xs-6 col-md-6">
+    <div class="thumbnail">
+      <img src="img/about/leo.jpg">
+      <div class="caption">
+        <h3>Leo Hellwich</h3>
+        <p>Vereinsgründer</p>
+        <p><a href="#" class="btn btn-default" role="button">Mehr Infos..</a></p>
+      </div>
+    </div>
+  </div>
+</div>
--- website_verein/WebContent/pages/base/head.php
+++ website_verein/WebContent/pages/base/head.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="de">
 	<head>
-		<meta charset="utf-8">
+		<!-- <meta charset="utf-8"> -->
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="description" content="">
 		<meta name="author" content="">
-		<link rel="icon" href="../../favicon.ico">
+		<link rel="icon" href="img/favicon.ico">
 
 		<title>Bierfreunde e.V.</title>
 
