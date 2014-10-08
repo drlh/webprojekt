26075ba23f89b6cc6bb0f3ea6c1408a014beaaae
drlh <leo.hellwich@yahoo.de>
2014-10-08 23:24:30 +0200

    About - Jquery Funktionen

--- website_verein/WebContent/js/functions.js
+++ website_verein/WebContent/js/functions.js
@@ -1,17 +1,30 @@
 // JS - Script für eigene Funktionen
 
-/**
- * Diese Funktion ermöglicht es beim Scrollen der Seite nach dem Bild die Navigation an den oberen Rand anzudocken
- */
 $(document).ready(function() {
+
+	/**
+	 * Diese Funktion ermöglicht es beim Scrollen der Seite nach dem Bild die
+	 * Navigation an den oberen Rand anzudocken
+	 */
+	var top = 200;
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > top) {
+			$('.navbar').addClass('navbar-fixed-top');
+		} else {
+			$('.navbar').removeClass('navbar-fixed-top');
+		}
+	});
 	
-  var top = 200;
-  $(window).scroll(function() {
-    if($(this).scrollTop() > top) {
-      $('.navbar').addClass('navbar-fixed-top');
-    } else {
-      $('.navbar').removeClass('navbar-fixed-top');
-    }
-  });
-  
+	
+	/**
+	 * Diese Funktion setzt einen Evnethandler wenn ein bestimmter Button gedrückt wird und blendet das 
+	 * nachfolgende Attribut ein
+	 */
+	$('#about-leo-btn').click( function() {
+		$("#about-leo").slideToggle();
+	});
+	$('#about-yannick-btn').click( function() {
+		$("#about-yannick").slideToggle();
+	});
 });
+
--- website_verein/WebContent/pages/about.php
+++ website_verein/WebContent/pages/about.php
@@ -1,29 +1,54 @@
 <div class="page-header">
-  <h1>Über Uns  <small> - Wir stellen uns vor</small></h1>
+	<h1>Über Uns <small> - Wir stellen uns vor</small></h1>
 </div>
 <div>
-    <p>Wir sind Bierliebhaber durch und durch. Der Grund weshalb wir diesen Verein gegründet haben ist einfach. <br>
-    Wir wollen die Freude am Biertrinken mit allen Mitmenschen teilen. </p>
+	<p>
+		Wir sind Bierliebhaber durch und durch. Der Grund weshalb wir diesen Verein gegründet haben ist einfach.
+		<br>
+		Wir wollen die Freude am Biertrinken mit allen Mitmenschen teilen.
+	</p>
 </div>
 <div class="row">
-  <div class="col-xs-6 col-md-6">
-    <div class="thumbnail">
-      <img src="img/about/yannick.jpg">
-      <div class="caption">
-        <h3>Yannick Sarnoch</h3>
-        <p>Vereinsgründer</p>
-        <p><a href="#" class="btn btn-default" role="button">Mehr Infos..</a></p>
-      </div>
-    </div>
-  </div>
-  <div class="col-xs-6 col-md-6">
-    <div class="thumbnail">
-      <img src="img/about/leo.jpg">
-      <div class="caption">
-        <h3>Leo Hellwich</h3>
-        <p>Vereinsgründer</p>
-        <p><a href="#" class="btn btn-default" role="button">Mehr Infos..</a></p>
-      </div>
-    </div>
-  </div>
+	<div class="col-xs-6 col-md-6">
+		<div class="thumbnail">
+			<img src="img/about/yannick.jpg">
+			<div class="caption">
+				<h3>Yannick Sarnoch</h3>
+				<p>
+					Vereinsgründer
+				</p>
+				<p>
+					<button id="about-yannick-btn" type="button" class="btn btn-default" role="button">
+						Mehr Infos..
+					</button>
+				</p>
+				<div id="about-yannick">
+					<p>
+						hier kommen mehr Infos rein
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="col-xs-6 col-md-6">
+		<div class="thumbnail">
+			<img src="img/about/leo.jpg">
+			<div class="caption">
+				<h3>Leo Hellwich</h3>
+				<p>
+					Vereinsgründer
+				</p>
+				<p>
+					<button id="about-leo-btn" type="button" class="btn btn-default" role="button" onclick="toggleABoutDetails("leo")">
+						Mehr Infos..
+					</button>
+				</p>
+				<div id="about-leo">
+					<p>
+						hier kommen mehr Infos rein
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
